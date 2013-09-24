3a491b254a40b90f09d6f83ab68a1f9783629bc7
gglin <george.g.lin@gmail.com>
2013-09-24 10:48:47 -0400

    Adds youtube URL migration to songs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130716023530) do
+ActiveRecord::Schema.define(:version => 20130924144627) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -49,10 +49,11 @@ ActiveRecord::Schema.define(:version => 20130716023530) do
     t.string   "name"
     t.integer  "artist_id"
     t.integer  "genre_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "album_id"
     t.integer  "track"
+    t.string   "youtube_url"
   end
 
 end
